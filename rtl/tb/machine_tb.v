6b20a26212c0301a68c5c2bc02db39d9e8974dea
vasu <vasucp1207@gmail.com>
2025-04-23 04:46:12 +0530

    update path for submodule

--- rtl/tb/machine_tb.v
+++ rtl/tb/machine_tb.v
@@ -31,7 +31,7 @@ module machine_tb;
     if (!$value$plusargs("memfile=%s", memfile)) begin
       memfile = "memory.list"; // default fallback
     end
-    $readmemh(memfile, m_machine.m_ram.mem);
+    $readmemh("../../memory.list", m_machine.m_ram.mem);
     $dumpfile("machine.vcd");
     $dumpvars(0, m_machine);
 
